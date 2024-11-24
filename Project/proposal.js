613e8826b8a2950592bd33fa03a3e140474963e4
ハッサン <aawaramalik007@gmail.com>
2024-11-24 17:19:45 +0500

    Add files via upload

---
+++ Project/proposal.js
@@ -0,0 +1,7 @@
+document.getElementById('yesButton').addEventListener('click', function() {
+    alert('Yay! I love you!');
+});
+
+document.getElementById('noButton').addEventListener('click', function() {
+    alert('Oh no! Its the wrong option');
+});
